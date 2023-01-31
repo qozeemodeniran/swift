248b608fd8682155865272a371ef5d571935a80f
qozeemodeniran <Lastborn231>
2023-01-30 19:56:06 -0500

    studied Concurrency

---
+++ concurrency/concurrency1.swift
@@ -0,0 +1,39 @@
+/*
+us 'async' to mark function that runs asynchronuously
+*/
+
+func fetchUserID(from server: String) async -> Int {
+    if server == "primary" {
+        return 97
+    }
+    return 501
+}
+
+/*
+mark a call to an asynchronous function by writing 'await' in front of it
+*/
+func fetchUsername(from server: String) async -> String {
+    let userID = await fetchUserID(from: server)
+    if userID == 501 {
+        return "Qozeem Odeniran"
+    }
+    return "Guest"
+}
+
+/*
+Use 'async let' to call an asynchronous function, letting it run in parallel with other asynchronous code. When you use the value it returns, write await.
+*/
+func connectUser(to server: String) async {
+    async let userID = fetchUserID(from: server)
+    async let username = fetchUsername(from: server)
+    let greeting = await "Hello \(username), user ID \(userID)"
+
+    print(greeting)
+}
+
+/*
+Use Task to call asynchronous functions from synchronous code, without waiting for them to return.
+*/
+Task {
+    await connectUser(to: "primary")
+}
