0ea69a31fd416b8e15a9c3c9104ae8694c5afceb
qozeemodeniran <Lastborn231>
2023-02-09 06:53:19 -0500

    started The Basics, and studied constant and variable. Now moving to Naming Constants and Variables

---
+++ basics/constant_and_variables.swift
@@ -0,0 +1,12 @@
+// use 'let' to declare a constant
+let max_num_of_login_attempts = 10
+
+// declaring multiple constants on a single line
+let const1=10, const2=20, const3=30
+
+// use 'var' to declare a variable
+var current_login_attempt = 0
+
+// declaring multiple variables on a single line
+var var1=40, var2=50, var3=60
+
---
+++ basics/type_annotation.swift
@@ -0,0 +1,24 @@
+// Type Annotation: indicating the type of value that a constant or variable should store
+
+// for variable: Single
+var age: Int
+age = 28
+
+// for variable: Multiple
+var  projects, experience: Int
+projects=103
+experience=3
+
+// for constant: Single
+let name: String
+name = "Qozeem Odeniran"
+
+// for constant: Multiple
+let major1, major2, degree1, degree2, job: String
+major1="Computer Science"
+major2="Information Technology"
+degree1="Bachelors"
+degree2="Matsers"
+job="Software Engineer"
+
+print("My name is \(name), and I am \(age) years old. In my \(experience) years working as a \(job), I have worked on a total of \(projects). I had my \(degree1) in \(major1), and my \(degree2) in \(major2).")
