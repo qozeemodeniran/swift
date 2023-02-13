e61acaa7caa479d5c5c002d95f8b402449e5d957
qozeemodeniran <Lastborn231>
2023-02-13 07:15:20 -0500

    studying The Basics: studied printing, integer bounds, type safety and type inference. Moving to 'Numeric Literals'

---
+++ basics/int_bounds.swift
@@ -0,0 +1,5 @@
+let minVal = UInt16.min
+let maxVal = UInt16.max
+
+print(minVal)
+print(maxVal)
---
+++ basics/print.swift
@@ -0,0 +1,5 @@
+let name: String
+name = "Qozeem Odeniran"
+
+// print(name, terminator: "") // would not add a newline after print
+print(name) // would add a new line after print
