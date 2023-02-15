83256d316e78a863ae21ca404caf6308390e3c28
qozeemodeniran <Lastborn231>
2023-02-15 15:03:43 -0500

    studied 'Numeric Literals', moving to 'Numeric Type Conversion'

---
+++ basics/numeric_literal.swift
@@ -0,0 +1,6 @@
+let decimalInteger = 17
+let binaryInteger = 0b10001
+let octalInteger = 0o21
+let hexadecimalInteger = 0x11
+
+print("Decimal = \(decimalInteger) \r\nBinary = \(binaryInteger) \r\nOctal = \(octalInteger) \r\nHexadecimal = \(hexadecimalInteger)")
